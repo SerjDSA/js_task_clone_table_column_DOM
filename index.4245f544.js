534cb33396d71fa5f61e91ef11ea3b1da2819b84
SerjDSA <serjdsa7@gmail.com>
2024-06-12 06:46:52 +0300

    make build

---
+++ index.4245f544.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("thead"),t=document.querySelector("tbody"),o=document.querySelector("tfoot");function r(e,t){e.querySelectorAll("tr").forEach(function(e){var o=e.cells[t-1].cloneNode(!0);!function(e,t){e.insertBefore(t,e.lastElementChild)}(e,o)})}r(e,2),r(t,2),r(o,2);
+//# sourceMappingURL=index.4245f544.js.map
